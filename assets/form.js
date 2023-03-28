b2efb574dddb5242748eb84eadae892513b2f3ff
Michael Fehrenbach <mf@michaelfehrenbach.com>
2023-03-28 19:03:54 -0400

    Update query string on change/submit.

--- assets/form.js
+++ assets/form.js
@@ -1,9 +1,11 @@
-// Check for query/params in the URL
+// First, check for query/params in the URL
 if (window.location.search) {
-	let params = new URLSearchParams(window.location.search) // Parse them into a variable
+	// Parse them into a variable
+	let urlParams = new URLSearchParams(window.location.search)
 
 	// Loop through each key/value pair
-	params.forEach((value, key) => {
+	urlParams.forEach((value, key) => {
+		// Find them by their ID
 		let inputOrSelect = document.getElementById(key)
 
 		if (inputOrSelect) {
@@ -19,6 +21,37 @@ if (window.location.search) {
 		}
 	})
 }
+
+
+
+// Find your form
+const formElement = document.querySelector('form')
+
+// Function to update the URL from the form
+const updateQueryString = () => {
+	let formParams = new FormData(formElement) // Get the form data
+
+	formParams = new URLSearchParams(formParams) // Make it into params
+	formParams = formParams.toString() // And then into a string.
+
+	// You could also write this as:
+	// let formParams = new URLSearchParams(new FormData(formElement)).toString()
+
+	// Update the URL with the params at the end
+	window.history.replaceState(null, null, '?' + formParams)
+}
+
+// When the form is submitted
+formElement.onsubmit = () => {
+	event.preventDefault() // Don’t actually submit (which would refresh)
+
+	updateQueryString() // But update the query string
+}
+
+// Or even any time it is modified!
+formElement.oninput = () => updateQueryString()
+
+
 
 // Clear any query/params from the URL
 document.getElementById('reset-params').onclick = () => {
